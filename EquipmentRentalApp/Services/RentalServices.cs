9646906ac0ed44daddccb859065596075e1e0813
Damian Walczuk <05damiankv05@gmail.com>
2026-04-05 23:08:03 +0200

    Implement equipment renting logic

--- EquipmentRentalApp/Services/RentalServices.cs
+++ EquipmentRentalApp/Services/RentalServices.cs
@@ -18,4 +18,40 @@ public class RentalService
         _userRepository = userRepository;
         _rentalRepository = rentalRepository;
     }
+    
+    public Rental RentEquipment(int rentalId, int equipmentId, int userId, int days)
+    {
+        var equipment = _equipmentRepository.GetById(equipmentId);
+        var user = _userRepository.GetById(userId);
+
+        if (equipment == null)
+            throw new Exception("Equipment not found");
+
+        if (user == null)
+            throw new Exception("User not found");
+
+        if (!equipment.IsAvailable)
+            throw new Exception("Equipment is not available");
+
+        var activeRentalsCount = _rentalRepository
+            .GetActive()
+            .Count(r => r.User.Id == userId);
+
+        if (activeRentalsCount >= user.MaxActiveRentals)
+            throw new Exception("User reached rental limit");
+
+        var rental = new Rental(
+            rentalId,
+            equipment,
+            user,
+            DateTime.Now,
+            DateTime.Now.AddDays(days)
+        );
+
+        equipment.IsAvailable = false;
+
+        _rentalRepository.Add(rental);
+
+        return rental;
+    }
 }
