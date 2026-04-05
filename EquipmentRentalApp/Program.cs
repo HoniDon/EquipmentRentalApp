d05741491c82210eeba0e00611c3f581f0bcc00a
Damian Walczuk <05damiankv05@gmail.com>
2026-04-05 23:25:02 +0200

    Add example in Program.cs

--- EquipmentRentalApp/Program.cs
+++ EquipmentRentalApp/Program.cs
@@ -1,1 +1,83 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EquipmentRentalApp.Models;
+using EquipmentRentalApp.Repositories;
+using EquipmentRentalApp.Services;
+
+var equipmentRepo = new EquipmentRepository();
+var userRepo = new UserRepository();
+var rentalRepo = new RentalRepository();
+
+var rentalService = new RentalService(
+    equipmentRepo,
+    userRepo,
+    rentalRepo
+);
+
+Console.WriteLine("=== ADD EQUIPMENT ===");
+
+equipmentRepo.Add(new Laptop(1, "eq1", 10, "Ryzen 5 5700x3d"));
+equipmentRepo.Add(new Projector(2, "eq2", 300, "HD"));
+equipmentRepo.Add(new Camera(3, "eq3", 50, true));
+
+foreach (var e in rentalService.GetAllEquipment())
+{
+    Console.WriteLine($"{e.Id} {e.Name} available={e.IsAvailable}");
+}
+
+Console.WriteLine("\n=== ADD USERS ===");
+
+userRepo.Add(new Student(1, "Damian", "Kraj"));
+userRepo.Add(new Employee(2, "Ewa", "Wrek"));
+
+Console.WriteLine("users added");
+
+Console.WriteLine("\n=== RENT EQUIPMENT ===");
+
+var rental1 = rentalService.RentEquipment(
+    rentalId: 1,
+    equipmentId: 1,
+    userId: 1,
+    days: 7
+);
+
+Console.WriteLine("laptop rented");
+
+Console.WriteLine("\n=== TRY INVALID RENT ===");
+
+try
+{
+    rentalService.RentEquipment(
+        rentalId: 2,
+        equipmentId: 1,
+        userId: 2,
+        days: 3
+    );
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("\n=== RETURN ON TIME ===");
+
+rentalService.ReturnEquipment(1);
+
+Console.WriteLine("equipment returned");
+
+Console.WriteLine("\n=== CREATE LATE RENTAL ===");
+
+var rental2 = rentalService.RentEquipment(
+    rentalId: 3,
+    equipmentId: 2,
+    userId: 2,
+    days: 0
+);
+
+Thread.Sleep(2000);
+
+rentalService.ReturnEquipment(3);
+
+Console.WriteLine($"penalty = {rental2.Penalty}");
+
+Console.WriteLine("\n=== REPORT ===");
+
+Console.WriteLine(rentalService.GenerateReport());
