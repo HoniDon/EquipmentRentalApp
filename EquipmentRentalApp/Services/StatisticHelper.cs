669d72547816b49d37af0949539446fc865f7509
Damian Walczuk <05damiankv05@gmail.com>
2026-04-05 23:35:30 +0200

    Handle empty array in average calculation

--- EquipmentRentalApp/Services/StatisticHelper.cs
+++ EquipmentRentalApp/Services/StatisticHelper.cs
@@ -4,7 +4,7 @@ public static class StatisticsHelper
 {
     public static double CalculateAverage(int[] values)
     {
-        if (values.Length == 0)
+        if (values == null || values.Length == 0)
             return 0;
 
         return values.Average();
